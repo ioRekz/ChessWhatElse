d6b8c7e793a8ae77dd2042f71b9a84332b088631
Jeremy Vuillermet <jeremy.vuillermet@gmail.com>
2012-02-29 09:04:20 +0100

    blitz on at blitz/:table1/:table2, normal at normal/:table
    removed useless alerts
    
    rm chosen

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,36 +5,88 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.libs.iteratee._
+import play.api.libs.json._
 
 object Application extends Controller {
   
   val helloForm = Form(
-      "game" -> nonEmptyText 
+		"game" -> nonEmptyText 
   )
   
-  def index(game : String) = Action { implicit request =>
-    Ok(views.html.index(game))
+  def index(game : String, opp : Option[String]) = Action { implicit request =>
+    Ok(views.html.index(game, opp))
   }
   
+	def normal(game: String) = Action { implicit request =>
+		Ok(views.html.index(game,None))
+	}
+	
+	def blitz(game: String, opp : String) = Action { implicit request => 
+		Ok(views.html.index(game,Some(opp)))
+	}
+	
   def hello = Action { Ok(views.html.hello(helloForm)) }
   
   def createGame = Action { implicit request =>
 		val game = helloForm.bindFromRequest.get
-		Redirect(routes.Application.index(game))
+		Ok(views.html.index(game,None))
   }
   
-  //val out = Enumerator.imperative[String](onStart = {println("enumerator created")})
   
-  def joinGame = WebSocket.using[String] { implicit request =>
+  var players = List[(String,PushEnumerator[JsValue])]()
+  var blitzz = List[(String,String)]()
+  
+  def joinGame(game : String, game2 : Option[String]) = WebSocket.using[JsValue] { implicit request =>
 		 // Log events to the console
-		  val in = Iteratee.foreach[String](println).mapDone { _ =>
-			println("Disconnected")
+		  val in = Iteratee.foreach[JsValue]{ msg =>
+				val currgame = (msg \ "game").as[String]
+				(msg \ "dead").asOpt[String] match {
+					case Some(dead) => {
+						val duo = blitzz find { tables  => tables._1 == currgame || tables._2 == currgame }
+						//si dead est envoye, duo ne peut pas etre None. TODO Refactor
+						val otherTable = if(duo.get._1 == currgame) duo.get._2 else duo.get._1
+
+						players.find ( _._1 == otherTable ) match {
+							case Some(table) => table._2.push(
+								JsObject(
+									Seq(
+									"help" -> JsString(dead)
+									)
+								))
+							case None => println("Can't play blitz by yourself")
+						}
+					}
+					
+					case None => println("pas de mort")
+				}
+				for(p <- players if p._1 == currgame ) {println(msg); p._2 push msg}
+		  }.mapDone { _ =>
+				println("Disconnected")
 		  }
-		  
+	
 		  // Send a single 'Hello!' message
-		  val out = Enumerator("Hello!")
-		  
-		  (in, out)
+		  players = (game, Enumerator.imperative[JsValue]()) +: players
+			game2 foreach { g2 =>
+				blitzz = (game,g2) :: blitzz
+			}
+			
+		  (in, players.head._2)
   }
+	
+	def blitzSock(game: String, game2 : String) = WebSocket.using[JsValue] { implicit request =>
+			val in = Iteratee.foreach[JsValue]{ msg =>
+				val s = (msg \ "game").as[String]
+				val board = (msg \ "board").as[String]
+				val lastdead = board.split(" ")(1)(0)
+				for(p <- players if p._1 == s ) {println(msg); p._2 push msg}
+		  }.mapDone { _ =>
+				println("Disconnected")
+		  }
+			
+			players = (game, Enumerator.imperative[JsValue]()) +: players
+			
+		  (in, players.head._2)
+	
+	}
   
 }
